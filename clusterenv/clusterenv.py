743dd37c0d1b25c48c7792be3fba48d16de221a2
Rodney Lafuente Mercado <Rodney.LafuenteMercado@ll.mit.edu>
2025-07-10 15:02:29 -0400

    change license

--- clusterenv/clusterenv.py
+++ clusterenv/clusterenv.py
@@ -2,7 +2,8 @@ from clusterenv.launchers import SlurmConfig
 
 class ClusterEnv():
     def __init__(self, env_config, config: SlurmConfig):
-        pass
+        self.env_config = env_config
+        self.config = config
 
     def launch(self):
         pass
